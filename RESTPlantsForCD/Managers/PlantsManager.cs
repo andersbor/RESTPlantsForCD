27662fddc0778807c88aeb5d872089b610bf14dc
Anders Børjesson <anbo@zealand.dk>
2021-10-04 20:00:30 +0200

    Add project files.

---
+++ RESTPlantsForCD/Managers/PlantsManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using RESTPlantsForCD.Models;
+
+namespace RESTPlantsForCD.Managers
+{
+    public class PlantsManager
+    {
+        private static int _nextId = 1;
+        private static readonly List<Plant> Data = new List<Plant>
+        {
+            new Plant {Id = _nextId++, Name="Sunflower"},
+            new Plant {Id = _nextId++, Name="Palm tree"}
+        };
+
+        public List<Plant> GetAll()
+        {
+            return Data;
+        }
+
+
+    }
+}
